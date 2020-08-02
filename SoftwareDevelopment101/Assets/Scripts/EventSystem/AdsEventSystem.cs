8fa85261381a99cb1dfca874d48d44a121cc7c3e
Halil Coşgun <cosgun.halil@gmail.com>
2020-08-03 01:19:37 +0300

    feat: add central event system
    # add adsEventSystem

---
+++ SoftwareDevelopment101/Assets/Scripts/EventSystem/AdsEventSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SD101.Common.Observer;
+using UnityEngine;
+
+public class AdsEventSystem : ISubject<SD101.Common.Event>
+{
+    private List<IObserver<SD101.Common.Event>> observers = new List<IObserver<SD101.Common.Event>>();
+
+    public void Register(IObserver<SD101.Common.Event> observer)
+    {
+        if (!observers.Contains(observer))
+        {
+            observers.Add(observer);
+        }
+    }
+
+    public void UnRegister(IObserver<SD101.Common.Event> observer)
+    {
+        if (observers.Contains(observer))
+        {
+            observers.Remove(observer);
+        }
+    }
+
+    public void AddEvent(SD101.Common.Event eventArgs)
+    {
+        for (int i = 0; i < observers.Count; i++)
+        {
+            observers[i].Notify(this, eventArgs);
+        }
+    }
+}
---
+++ SoftwareDevelopment101/Assets/Scripts/EventSystem/CentralEventManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CentralEventManager
+{
+    private static readonly CentralEventManager instance = new CentralEventManager();
+
+    private AdsEventSystem adsEventSystem = new AdsEventSystem();
+
+    static CentralEventManager()
+    {
+
+    }
+
+    private CentralEventManager()
+    {
+
+    }
+
+    public static CentralEventManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public AdsEventSystem GetUIStateEventManager()
+    {
+        return adsEventSystem;
+    }
+}
