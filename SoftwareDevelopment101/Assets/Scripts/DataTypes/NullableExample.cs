6da6b9769f7e26fe9561e5b7a7ccab31a3167566
Halil Coşgun <cosgun.halil@gmail.com>
2020-08-06 22:37:44 +0300

    feat: add nullable int example

---
+++ SoftwareDevelopment101/Assets/Scripts/DataTypes/NullableExample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SD101.Common;
+using UnityEngine;
+
+namespace SD101.Example.Nullable
+{
+    public class NullableExample : IExample
+    {
+        private int? nullableInt = null;
+
+        public void Execute()
+        {
+            if (nullableInt == null)
+            {
+                Debug.Log("nullable int is null!");
+            }
+        }
+    }
+}
+
+
