2ebb8f9900e72658ec50bd5ffcd40fd3f770b0a7
Halil Coşgun <cosgun.halil@gmail.com>
2021-01-19 22:01:47 +0300

    feat(mvc): Create floor data generator

---
+++ SoftwareDevelopment101/Assets/Scripts/ArchitecturalDesignPatterns/MVC/FloorDataGenerator.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace SD101.Example.MVC
+{
+    public class FloorDataGenerator
+    {
+        private const float minSizeLimit = 1;
+        private const float maxSizeLimit = 5;
+        public FloorData generateRandomFloorData() 
+        {
+            return new FloorData() {Size = new UnityEngine.Vector3(
+                UnityEngine.Random.Range(minSizeLimit, maxSizeLimit), 
+                UnityEngine.Random.Range(minSizeLimit, maxSizeLimit), 
+                UnityEngine.Random.Range(minSizeLimit, maxSizeLimit)) 
+            };
+        }
+    }
+}
+
