e3709a352e8f040bd9e1107dd9e41e401c098df3
Halil Coşgun <cosgun.halil@gmail.com>
2021-01-19 22:01:23 +0300

    feat(mvc): Create BuildingMVC sample script

---
+++ SoftwareDevelopment101/Assets/Scripts/ArchitecturalDesignPatterns/MVC/BuildingMVC.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace SD101.Example.MVC 
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class BuildingMVC : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            BuildingModel buildingModel = new BuildingModel();
+            var buildingView = new GameObject("BuildingView").AddComponent<BuildingView>();
+
+            BuildingController buildingController = new BuildingController(buildingModel, buildingView);
+
+            buildingView.AddFloor();
+            buildingView.AddFloor();
+            buildingView.RemoveFloor();
+        }
+    }
+}
+
+
